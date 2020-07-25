28dd3a85393e6bd5d3cfb38366d2bc872764e595
Alexander Barouch <ajbarouch@gmail.com>
2020-07-24 17:11:19 -0700

    Updates

---
+++ precache-manifest.915f6f7f9c8699822a0e062b234ff960.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69abd575af4001ab258f6bd93f45d51",
+    "url": "/keaptest/index.html"
+  },
+  {
+    "revision": "935ba5fbe037becd37e1",
+    "url": "/keaptest/static/css/main.44deb9ef.chunk.css"
+  },
+  {
+    "revision": "c5b597c7942fd2539e44",
+    "url": "/keaptest/static/js/2.38fe65e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keaptest/static/js/2.38fe65e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935ba5fbe037becd37e1",
+    "url": "/keaptest/static/js/main.3b20637c.chunk.js"
+  },
+  {
+    "revision": "534e14f86db0f5049682",
+    "url": "/keaptest/static/js/runtime-main.040a8da7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/keaptest/static/media/logo.5d5d9eef.svg"
+  }
+]);
